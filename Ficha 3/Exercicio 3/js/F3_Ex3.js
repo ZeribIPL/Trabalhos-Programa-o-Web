eca9f127a7567270f70e20392e523c5a7683f047
José Ribeiro <2190722@my.ipleiria.pt>
2019-10-24 17:57:13 +0100

    Trabalhos de Programação Web

---
+++ Ficha 3/Exercicio 3/js/F3_Ex3.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var imagem=null;
+
+function escondeMostra(){
+    console.log("esconde/mostra");
+
+    var visibilidade=getComputedStyle(imagem).getPropertyValue("visibility");
+    console.log(visibilidade);
+    if(visibilidade==="visible") {
+        imagem.style.visibility="hidden";
+    } else {
+        imagem.style.visibility="visible";
+    }
+}
+
+function esconde(){
+    console.log("esconde");
+    imagem.style.display="none";
+}
+
+function mostra(){
+    console.log("mostra");
+    imagem.style.display="inline-block";
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    imagem=document.getElementsByTagName("img")[0];
+  var botao=document.getElementById("botao");
+botao.addEventListener("mouseover", mostra);  
+});
